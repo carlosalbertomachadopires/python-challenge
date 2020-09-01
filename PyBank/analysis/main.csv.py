743afb38b54f127ea7a2b0732d9463969497e78f
CPDEZ08 <CPDEZ08@outlook.com>
2020-09-01 22:54:08 +0000

    Total Months calculation code and selection of the best alternative to read the csv file

--- PyBank/analysis/main.csv.py
+++ PyBank/analysis/main.csv.py
@@ -4,60 +4,40 @@ import csv
 
 path = os.path.join("..", "..", "Instructions", "PyBank", "Resources", "budget_data.csv")
 
-months = 0
-
+# with open(path, "r") as file:
+#     header = file.readline()
+#     print("This is the header")
+#     print(header)
+    
+#     data = file.readlines()    
+#     print("This is the data")
+#     print(data)
+    
+#     print([row.strip().split(",") for row in data])
+    
 with open(path, "r") as file:
-    dict_reader = csv.DictReader(file)
-    for row in dict_reader:
-        months = months + 1
-    print("Months Count:", months)
+    csv_reader = csv.reader(file)
     
-            
-       
-#         changes = []
-#         for i in range(1, len(data)):
-#             last_period_row = data[i-1]
-#             current_period_row = data[i]
+    header = next(csv_reader)
+    print(header)
+    
+    data = list(csv_reader)
+    print(data)
+    
+   
+    changes = []
+    for i in range(1, len(data)):
+        last_period_row = data[i-1]
+        current_period_row = data [i]
         
-#             change = (last_period_row + current_period_row)
+        change = i + 1       
+        changes.append(change)
+        print(f" Total Months: {change}")
         
-#             changes.append(change)
+  
 
-      
-# #         print(changes)
-        
 
-# header = PyBank.readline()
-
-# data = print([element.strip().split(",") for element in PyBank])
-
-# print(new_list)
-
-# count = len(new_list)
-
-# new_list[0]
-
-# total = 0
-# for e in new_list:
-#     total = total + 1
-
-# #     print(type(PyBank))
-# new_list[0][1]
-    
-# #     header = file.readline()
-# # #     print("This is the header")
-# # #     print(header)
-    
-# #     data = file.readlines()
-# # # #     data = int(data)
-# # # #     print("This is the data")
-# # #     print(type(data))
-# # #     print(data)
-    
-    
-# # #      print([element.strip().split(" ")for element in data])
-    
-#       dict_reader = csv.DictReader(file)
-#       for row in dict_reader:
+# with open(path, "r") as file:
+#     dict_reader = csv.DictReader(file)
+#     for row in dict_reader:
 #         print(dict(row))
-               
