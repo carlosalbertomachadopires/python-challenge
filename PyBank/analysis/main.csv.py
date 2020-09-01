06311676cf3a51a214bdd711fd75d493f60a5490
CPDEZ08 <CPDEZ08@outlook.com>
2020-09-01 03:58:27 +0000

    Months code count

--- PyBank/analysis/main.csv.py
+++ PyBank/analysis/main.csv.py
@@ -4,9 +4,60 @@ import csv
 
 path = os.path.join("..", "..", "Instructions", "PyBank", "Resources", "budget_data.csv")
 
+months = 0
+
 with open(path, "r") as file:
+    dict_reader = csv.DictReader(file)
+    for row in dict_reader:
+        months = months + 1
+    print("Months Count:", months)
     
-    header = file.readLine()
-    print("This the header")
-    print(header)
+            
+       
+#         changes = []
+#         for i in range(1, len(data)):
+#             last_period_row = data[i-1]
+#             current_period_row = data[i]
+        
+#             change = (last_period_row + current_period_row)
+        
+#             changes.append(change)
+
+      
+# #         print(changes)
+        
+
+# header = PyBank.readline()
+
+# data = print([element.strip().split(",") for element in PyBank])
+
+# print(new_list)
+
+# count = len(new_list)
+
+# new_list[0]
+
+# total = 0
+# for e in new_list:
+#     total = total + 1
+
+# #     print(type(PyBank))
+# new_list[0][1]
     
+# #     header = file.readline()
+# # #     print("This is the header")
+# # #     print(header)
+    
+# #     data = file.readlines()
+# # # #     data = int(data)
+# # # #     print("This is the data")
+# # #     print(type(data))
+# # #     print(data)
+    
+    
+# # #      print([element.strip().split(" ")for element in data])
+    
+#       dict_reader = csv.DictReader(file)
+#       for row in dict_reader:
+#         print(dict(row))
+               
