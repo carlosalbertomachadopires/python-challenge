23782ebcad6c74cae2ae7b1daa798748578bbe47
CPDEZ08 <CPDEZ08@outlook.com>
2020-09-02 07:36:35 +0000

    Updated codes considering: Total Monts, Total Net Profits, Net Profit Average and Greatest Increase on Net Profit

--- PyBank/analysis/main.csv.py
+++ PyBank/analysis/main.csv.py
@@ -4,40 +4,47 @@ import csv
 
 path = os.path.join("..", "..", "Instructions", "PyBank", "Resources", "budget_data.csv")
 
-# with open(path, "r") as file:
-#     header = file.readline()
-#     print("This is the header")
-#     print(header)
-    
-#     data = file.readlines()    
-#     print("This is the data")
-#     print(data)
-    
-#     print([row.strip().split(",") for row in data])
-    
+
+Months = []
+Profit_Losses = []
+Avg = []
+Net_Profit = []
+
 with open(path, "r") as file:
     csv_reader = csv.reader(file)
     
     header = next(csv_reader)
-    print(header)
+#     print(header)
     
-    data = list(csv_reader)
-    print(data)
-    
-   
-    changes = []
-    for i in range(1, len(data)):
-        last_period_row = data[i-1]
-        current_period_row = data [i]
+    for row in csv_reader:
+        Months.append(str(row[0]))
+        Profit_Losses.append(int(row[1]))
         
-        change = i + 1       
-        changes.append(change)
-        print(f" Total Months: {change}")
+# Total Months
+
+        Total_Months = len(Months)
+        print(f" Total Months : {Total_Months}")
         
-  
-
-
-# with open(path, "r") as file:
-#     dict_reader = csv.DictReader(file)
-#     for row in dict_reader:
-#         print(dict(row))
+# Total Net Profit 
+        
+    for i in range(1, len(Profit_Losses)):
+        Previous_PL = int(Profit_Losses[i-1])
+        Current_PL = int(Profit_Losses[i])
+        
+        change = (Current_PL - Previous_PL)
+        Net_Profit.append(change)
+        Total_Net_Profit = sum(Profit_Losses)
+        print(f" Total_Net Profit: {Total_Net_Profit}")
+        
+# Net Profit Average
+        
+        Total_change = int(sum(Net_Profit))
+        Net_Profit_Average = Total_change/len(Net_Profit)
+        print(f"Net Profit Average: {Net_Profit_Average}")
+        
+# Greatest Increase on Net Profit
+        Greatest_Increase_Net_Profit = max(Net_Profit)
+        print(f"Greatest Increase on Net_Profit: {Greatest_Increase_Net_Profit}")
+        
+        
+        
