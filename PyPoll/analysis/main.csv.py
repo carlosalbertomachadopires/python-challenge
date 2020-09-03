de4889317492bc18bc50a42513fda81e2b407e44
CPDEZ08 <CPDEZ08@outlook.com>
2020-09-03 22:42:40 +0000

    Total number of votes and complete list of candidates who received votes

--- PyPoll/analysis/main.csv.py
+++ PyPoll/analysis/main.csv.py
@@ -1,13 +1,15 @@
 import os
 import csv
-
+# from operator import itemgetter, attrgetter
+from enum import Enum, unique
 
 path = os.path.join("..", "..", "Instructions", "PyPoll", "Resources", "election_data.csv")
 
-# votes_cast = {}
-# candidate = 1
+votes_quant = []
+candidate_names = []
 total_votes = 0
-# votes = 1
+# votes = []
+
 
 with open(path, "r") as file:
     dict_reader = csv.DictReader(file)
@@ -17,21 +19,64 @@ with open(path, "r") as file:
 # The total number of votes cast 
     
         votes_cast = str(row["Voter ID"])
-        total_votes +=1     
-        print(total_votes)
+        votes_quant.append(votes_cast)    
+        total_quant_votes = len(votes_quant)
+        print(total_quant_votes)
         break
+        
+# A complete list of candidates who received votes
+        
+    for row in dict_reader:
+        
+        candidate = (row["Candidate"])
+        candidate_names.append(candidate)
+        def candidate_with_votes(candidate_names, votes_quant):
+            if candidate_names != None and votes_quant == None:
+                return False
+            else:
+                return candidate_names
+        result = candidate_with_votes(candidate_names, votes_quant)
+        print(f"Candidate with votes: {result}")
+        break
+           
+    for row in dict_reader:
+        
+# The percentage of votes each candidate won
+
+        print(list(enumerate(candidate_names, start=0)))
+        break
+            
+
+#         break
+#         print(candidate_names)
+#         break
+        
+        
+        
+        
+        county = str(row["County"])
+#         print(county)
+#         break
+        
+        
+#         candidate_votes = (candidate, votes_cast, county)
+#         votes.append(candidate_votes)
+#         print(votes)
+#         break
+#         print(candidate_votes)
+#         break
        
     
 # A complete list of candidates who received votes
         
-        candidate = (row["Candidate"])
-        candidate_votes = (candidate + votes_cast)
-        print(candidate_votes)
-        break
+#         candidate_sort = sorted(votes, key=lambda votes:[0])
+#         print(candidate_sort)
+#         candidate_votes = (candidate + votes_cast)
+#         print(candidate_votes)
+#         break
         
            
         
-# The percentage of votes each candidate won
 
 # The total number of votes each candidate won
 
